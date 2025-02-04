5bda2a40f2af9d57217cd6acf9dbfb3d0694f639
RAKIB MAHMUD <77047747+rakibmahmudanik@users.noreply.github.com>
2025-02-04 13:55:11 +0600

    Add header Sticky function with resposive

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const menu = document.getElementById("dropdownMenu");
   const links = document.querySelectorAll(".nav-bar a");
 
+  const header = document.querySelector("header"); // Header element select
+  const scrollThreshold = 200; //  scroll korle effect apply count
+
   button.addEventListener("click", function () {
     if (menu.style.display === "block") {
       menu.style.display = "none";
@@ -28,5 +31,19 @@ document.addEventListener("DOMContentLoaded", function () {
       // Add active class to the clicked link
       this.classList.add("nav-active");
     });
+  });
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > scrollThreshold) {
+      header.classList.add("sticky-header");
+      header.classList.add("animate");
+      if (window.innerWidth <= 768) {
+        header.classList.remove("sticky-header");
+        header.classList.remove("animate");
+      }
+    } else {
+      header.classList.remove("sticky-header");
+      header.classList.remove("animate");
+    }
   });
 });
